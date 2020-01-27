eaa37803d2a25b13b5c76d26309aa8c4b18b9f38
wintervt <mila23ster@gmail.com>
2020-01-27 20:32:00 +0000

    Reviews Component and Database Seed

---
+++ resources/js/bookable/ReviewList.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="p-3">
+		<h6 class="text-uppercase text-secondary font-weight-bolder pt-3">Review List</h6>
+
+		<div class="border-bottom d-none d-md-block" v-for="number in 3" :key="number">	
+
+			<div class="row pt-3">	
+				<div class="col-md-6">Milan Toth</div>
+				<div class="col-md-6 d-flex justify-content-end">STAR RATING</div>
+			</div>
+
+			<div class="row">	
+				<div class="col-12">Added 5 minutes ago</div>
+			</div>
+
+			<div class="row pb-3">	
+				<div class="col-12">
+					Content of the review
+				</div>
+			</div>
+
+		</div>
+	</div>
+</template>
