472aaf3ea875eddaa84a040209a0eedc31815d69
wintervt <mila23ster@gmail.com>
2020-01-28 21:07:45 +0000

    ReviewList component

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -23,6 +23,9 @@
 
 <script>
 	export default {
+		props: {
+			bookableId: String
+		},
 		data() {
 			return {
 				from: null,
@@ -37,7 +40,7 @@
 			this.loading = true;
 			this.errors = null;
 			
-			axios.get(`/api/bookables/${this.$route.params.id}/availability?from=${this.from}&to=${this.to}`
+			axios.get(`/api/bookables/${this.bookableId}/availability?from=${this.from}&to=${this.to}`
 				).then(response => {
 					this.status = response.status;
 				}).catch(error => {
--- resources/js/bookable/ReviewList.vue
+++ resources/js/bookable/ReviewList.vue
@@ -2,23 +2,58 @@
 	<div class="p-3">
 		<h6 class="text-uppercase text-secondary font-weight-bolder pt-3">Review List</h6>
 
-		<div class="border-bottom d-none d-md-block" v-for="number in 3" :key="number">	
+		<div v-if="loading">Loading...</div>
 
-			<div class="row pt-3">	
-				<div class="col-md-6">Milan Toth</div>
-				<div class="col-md-6 d-flex justify-content-end">STAR RATING</div>
-			</div>
+		<div v-else>
 
-			<div class="row">	
-				<div class="col-12">Added 5 minutes ago</div>
-			</div>
+			<div class="border-bottom d-none d-md-block" v-for="(review, index) in reviews" :key="index">	
 
-			<div class="row pb-3">	
-				<div class="col-12">
-					Content of the review
+				<div class="row pt-3">	
+					<div class="col-md-6">Milan Toth</div>
+					<div class="col-md-6 d-flex justify-content-end">{{ review.rating }}</div>
 				</div>
-			</div>
 
+				<div class="row">	
+					<div class="col-12">{{ review.created_at | fromNow }}</div>
+				</div>
+
+				<div class="row pb-3">	
+					<div class="col-12">
+						{{ review.content }}
+					</div>
+				</div>
+
+			</div>
+			
 		</div>
+
 	</div>
 </template>
+
+
+<script>
+	import moment from "moment";
+
+	export default {
+		props: {
+			bookableId: String
+		},
+		data() {
+			return {
+				loading: false,
+				reviews: null
+			}
+		},
+		created() {
+			this.loading = true;
+			axios.get(`/api/bookables/${this.bookableId}/reviews`)
+			.then(response => this.reviews = response.data.data)
+			.then(() => (this.loading = false));
+		},
+		filters : {
+			fromNow(value) {
+				return moment(value).fromNow();
+			}
+		}
+	}
+</script>
